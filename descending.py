156e9806097b8275cc05e2e7e4096554dc838a70
RoySuwanda-21-046 <roysuwanda842@gmail.com>
2023-09-20 23:12:09 +0700

    test 1

---
+++ descending.py
@@ -0,0 +1,13 @@
+def insertionSort (listdata) :
+    for outIter in range (1, len(listdata)) :
+        print(listdata)
+        key = listdata[outIter]
+        ind= outIter
+        while(ind >0 and listdata[ind-1]>key) :
+            listdata[ind]=listdata[ind-1]
+            ind=ind-1
+            print ("inner =", listdata)
+        listdata[ind] = key
+    print("sorteddata=",listdata)
+a = [10,2,5,8,1,20,2,2,4]
+insertionSort (a)
